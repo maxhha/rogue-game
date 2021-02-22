1f27e35918111d1e6b738dc96a68c7cbd44c8ff4
devmaxhha <devmaxhha@gmail.com>
2021-02-22 18:29:21 +0300

    add player walking

--- src/player.rs
+++ src/player.rs
@@ -2,6 +2,7 @@ use crate::colors::{COLOR_BG, COLOR_PLAYER};
 use crate::draw::{BTerm, Draw, Point};
 use crate::field::FieldPosition;
 use crate::state::{State, Stepper};
+use bracket_terminal::prelude::VirtualKeyCode;
 
 const MOVE_CLOCK: f64 = 1.0;
 
@@ -19,6 +20,24 @@ impl Player {
             view_radius,
         }
     }
+
+    fn action(&mut self, world: &State, direction: (i32, i32)) {
+        let next_pos = self.pos + Point::from(direction);
+
+        if !world.field.is_wall(next_pos.x, next_pos.y) {
+            self.pos = next_pos
+        }
+    }
+
+    fn process_key(&mut self, world: &State, key: VirtualKeyCode) {
+        match key {
+            VirtualKeyCode::W => self.action(world, (0, -1)),
+            VirtualKeyCode::A => self.action(world, (-1, 0)),
+            VirtualKeyCode::S => self.action(world, (0, 1)),
+            VirtualKeyCode::D => self.action(world, (1, 0)),
+            _ => {}
+        }
+    }
 }
 
 impl FieldPosition for Player {
@@ -30,5 +49,18 @@ impl FieldPosition for Player {
 impl Draw for Player {
     fn draw(&self, ctx: &mut BTerm, pos: Point) {
         ctx.print_color(pos.x, pos.y, COLOR_PLAYER, COLOR_BG, "@")
+    }
+}
+
+impl Stepper for Player {
+    fn clock(&self) -> f64 {
+        self.clock
+    }
+
+    fn process(&mut self, world: &State, ctx: &BTerm) {
+        match ctx.key {
+            None => {}
+            Some(key) => self.process_key(world, key),
+        }
     }
 }
--- src/state.rs
+++ src/state.rs
@@ -4,12 +4,16 @@ use crate::player::Player;
 
 use bracket_pathfinding::prelude::*;
 use bracket_terminal::prelude::{BTerm, GameState};
+use std::cell::RefCell;
 use std::collections::HashSet;
+use std::rc::Rc;
 
 pub struct State {
-    field: Field,
+    pub field: Field,
     fov: HashSet<Point>,
-    player: Player,
+    player: Rc<RefCell<Player>>,
+    prev_player_pos: Point,
+    current_stepper: Option<Rc<RefCell<dyn Stepper>>>,
 }
 
 impl State {
@@ -18,7 +22,21 @@ impl State {
         let player = Player::new(Point::new(40, 12), 8);
         let fov = field_of_view_set(player.pos(), player.view_radius, &field);
 
-        State { field, fov, player }
+        State {
+            field,
+            fov,
+            current_stepper: None,
+            prev_player_pos: player.pos(),
+            player: Rc::new(RefCell::new(player)),
+        }
+    }
+
+    fn update_fov(&mut self) {
+        let player = self.player.borrow();
+        let pos = player.pos();
+
+        self.fov = field_of_view_set(pos, player.view_radius, &self.field);
+        self.prev_player_pos = pos;
     }
 }
 
@@ -26,14 +44,27 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
 
+        if let None = self.current_stepper {
+            let stepper = Rc::clone(&self.player);
+            self.current_stepper = Some(stepper)
+        }
+
+        if let Some(stepper) = &self.current_stepper {
+            stepper.borrow_mut().process(&self, ctx);
+        }
+
+        if self.prev_player_pos != self.player.borrow().pos() {
+            self.update_fov();
+        }
+
         self.field
             .draw_with_fov(ctx, &self.fov, Point::zero(), Point::zero());
 
-        self.player.draw(ctx, self.player.pos());
+        self.player.borrow().draw(ctx, self.player.borrow().pos());
     }
 }
 
 pub trait Stepper {
     fn clock(&self) -> f64;
-    fn process(&mut self, wld: &mut State);
+    fn process(&mut self, world: &State, ctx: &BTerm);
 }
